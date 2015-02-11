24992b8e0fdc964779bc8ba1f94ca1786734f545
alicechoi8 <choia8@bxscience.edu>
2015-02-11 17:07:12 -0500

    Update tailor.cs

--- system/scripts/shops/tailor.cs
+++ system/scripts/shops/tailor.cs
@@ -8,15 +8,15 @@ public class TailorShop : NpcShopScript
 {
     public override void Setup()
     {
-        Add("Tailoring", 60001);        // Tailoring Kit
-        Add("Tailoring", 60015);        // Cheap Finishing Thread
-        Add("Tailoring", 60015, 5);     // Cheap Finishing Thread
-        Add("Tailoring", 60031);        // Regular Silk Weaving Gloves
-        Add("Tailoring", 60019);        // Cheap Fabric
-        Add("Tailoring", 60019, 5);     // Cheap Fabric
-        Add("Tailoring", 60016);        // Common Finishing Thread
-        Add("Tailoring", 60016, 5);     // Common Finishing Thread
-        Add("Tailoring", 60017);        // Fine Finishing Thread
+        Add("Tailoring", 50409);        // pumpkin seed
+        Add("Tailoring", 50871);        // vamp coffin
+        Add("Tailoring", 95044);     //cessair V
+        Add("Tailoring", 95046);        // Regular Silk Weaving Gloves
+        Add("Tailoring", 95064);        // Cheap Fabric
+        Add("Tailoring", 95065);     // Cheap Fabric
+        Add("Tailoring", 95066);        // Common Finishing Thread
+        Add("Tailoring", 210319);     // muffler
+        Add("Tailoring", 19350);        // angel wing holy guardian
         Add("Tailoring", 60055);        // Fine Silk Weaving Gloves
         Add("Tailoring", 60017, 5);     // Fine Finishing Thread
         Add("Tailoring", 60046);        // Finest Silk Weaving Gloves

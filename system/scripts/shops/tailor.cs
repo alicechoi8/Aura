bfdfcde18f1bb50bde2910b57dc6fdf81b29ac2e
alicechoi8 <choia8@bxscience.edu>
2015-02-16 16:16:14 -0800

    Update tailor.cs

--- system/scripts/shops/tailor.cs
+++ system/scripts/shops/tailor.cs
@@ -10,21 +10,36 @@ public class TailorShop : NpcShopScript
     {
         Add("Tailoring", 50409);        // pumpkin seed
         Add("Tailoring", 50871);        // vamp coffin
-        Add("Tailoring", 95044);     //cessair V
-        Add("Tailoring", 95046);        // Regular Silk Weaving Gloves
-        Add("Tailoring", 95064);        // Cheap Fabric
-        Add("Tailoring", 95065);     // Cheap Fabric
-        Add("Tailoring", 95066);        // Common Finishing Thread
+        Add("Tailoring", 85797);     //golden drag statue
+        Add("Tailoring", 15927);        // odran outfit
+        Add("Tailoring", 75680);        // lux equip bag 
+        Add("Tailoring", 63258);     // dye amp
+        Add("Tailoring", 92652, 100);        // saga 2 gach
         Add("Tailoring", 210319);     // muffler
         Add("Tailoring", 19350);        // angel wing holy guardian
-        Add("Tailoring", 60055);        // Fine Silk Weaving Gloves
-        Add("Tailoring", 60017, 5);     // Fine Finishing Thread
-        Add("Tailoring", 60046);        // Finest Silk Weaving Gloves
-        Add("Tailoring", 60018);        // Finest Finishing Thread
-        Add("Tailoring", 60057);        // Fine Fabric Weaving Gloves
-        Add("Tailoring", 60056);        // Finest Fabric Weaving Gloves
-        Add("Tailoring", 60020);        // Common Fabric
-        Add("Tailoring", 60018);        // Finest Finishing Thread
-        Add("Tailoring", 60020, 5);     // Common Fabric
+        Add("Tailoring",  63033, 5);        // Black Dye Amp
+        Add("Tailoring", 91674, 10);     // shamala trans box
+        Add("Tailoring", 91678, 10);        // hero box
+        Add("Tailoring", 13167);        //DK set V
+        Add("Tailoring", 18685);        // Fine Fabric Weaving Gloves
+        Add("Tailoring", 17633);        // Finest Fabric Weaving Gloves
+        Add("Tailoring", 16933);        // Common Fabric
+        Add("Tailoring", 15495);        // trin set
+        Add("Tailoring", 17241);     // Common Fabric
+        Add("Tailoring", 16147);
+        Add("Tailoring", 18327);
+        Add("Tailoring", 18597); //diamond pattern helm
+        Add("Tailoring", 19137); //Myst Robe!!
+        Add("Tailoring", 91880); //divine duality cryst ball
+        Add("Tailoring", 18624); //google shades
+        Add("Tailoring", 15498); //BERCHED WEAR
+        Add("Tailoring", 67221); //black mask staff
+        Add("Tailoring", 85768); //nao stone
+        Add("Tailoring", 40657); //merlin shyllien knuckle
+        Add("Tailoring", 92034); //50 mil duc box
+        Add("Tailoring", 92028); //100 mil gold
+        Add("Tailoring", 92305);
+        Add("Tailoring", 54075);
+        
     }
 }
